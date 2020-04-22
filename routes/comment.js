fef2a09735233cd8f41ce58dc1c8f77c3b4429a8
Yuchen Lin <kloselyc@gmail.com>
2020-04-21 19:40:26 -0600

    change new database as old one was broken

--- routes/comment.js
+++ routes/comment.js
@@ -5,18 +5,15 @@ const json_helper = require("../helper/json_helper");
 const MongoClient = require("mongodb").MongoClient;
 
 const url =
-  "mongodb+srv://yuchen:1234@cluster0-xlcav.mongodb.net/post?retryWrites=true";
+  "mongodb+srv://yuchen:1234@cluster0-xlcav.mongodb.net/welifePost?retryWrites=true";
 
 const contentFilename = __dirname + "/../model/content.json";
 
 //pass database test
 router.get("/", (request, response) => {
-  MongoClient.connect(url, async function(err, client) {
+  MongoClient.connect(url, async function (err, client) {
     const db = client.db("post");
-    const collection = await db
-      .collection("content")
-      .find()
-      .toArray();
+    const collection = await db.collection("content").find().toArray();
 
     response.json(collection);
     client.close();
@@ -30,26 +27,23 @@ router.post("/", (request, response) => {
     name: request.body.name,
     comment: [],
     image: request.body.image,
-    video: request.body.video
+    video: request.body.video,
   };
   let curTime = new Date();
   let newComment = {
     comment: request.body.comment,
     timestamp: curTime.getTime(),
-    likes: 0
+    likes: 0,
   };
 
   //insertOne()
   newPost.comment.push(newComment);
 
-  MongoClient.connect(url, async function(err, client) {
+  MongoClient.connect(url, async function (err, client) {
     const db = client.db("post");
 
     const insert = await db.collection("content").insertOne(newPost);
-    const collection = await db
-      .collection("content")
-      .find()
-      .toArray();
+    const collection = await db.collection("content").find().toArray();
 
     response.json(collection);
     client.close();
@@ -60,10 +54,10 @@ router.post("/", (request, response) => {
 // add comment to exist post
 router.put("/:id", (request, response) => {
   let postId = request.params.id;
-  let curPost = content.find(element => element.id === postId);
-  let curIndex = content.findIndex(element => element.id === postId);
+  let curPost = content.find((element) => element.id === postId);
+  let curIndex = content.findIndex((element) => element.id === postId);
 
-  MongoClient.connect(url, async function(err, client) {
+  MongoClient.connect(url, async function (err, client) {
     const db = client.db("post");
 
     const replace = await db.collection("content").replaceOne(
@@ -74,14 +68,11 @@ router.put("/:id", (request, response) => {
           name: request.body.name,
           comment: request.body.comment,
           image: request.body.image,
-          video: request.body.video
-        }
+          video: request.body.video,
+        },
       }
     );
-    const collection = await db
-      .collection("content")
-      .find()
-      .toArray();
+    const collection = await db.collection("content").find().toArray();
 
     response.json(collection);
     client.close();
@@ -94,16 +85,13 @@ router.delete("/", (request, response) => {
 
   console.log(request.body);
 
-  MongoClient.connect(url, async function(err, client) {
+  MongoClient.connect(url, async function (err, client) {
     const db = client.db("post");
     console.log(request.body);
     const remove = await db.collection("content").deleteMany({
-      id: { $eq: id }
+      id: { $eq: id },
     });
-    const collection = await db
-      .collection("content")
-      .find()
-      .toArray();
+    const collection = await db.collection("content").find().toArray();
 
     response.json(collection);
     client.close();
